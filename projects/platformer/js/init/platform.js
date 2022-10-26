64ae4ade49859a1dad5cf4334691ca9a02af6ff1
cadetdhernandez <cadetdhernandez@nomma.net>
2022-10-26 17:49:51 +0000

    Day 2

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,12 @@
     createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
 
     // example:
-    createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
+    createPlatform(500, 500);
+    createPlatform(200, 600, 0.5);
+    createPlatform(20, 500);
+    createPlatform(500, 360);
+    createPlatform(200, 600);
+    
 
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
