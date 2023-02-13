fc09c9f95bcf406e845c50c285ed3294208373a4
cadetdhernandez <cadetdhernandez@nomma.net>
2023-02-13 18:30:29 +0000

    final

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,25 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-        var circle; 
-        var circle = [];
-
-
+        var circle;
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
-       function drawCircle() {
-        circle = draw.randomCircleInArea(canvas, true, true, "#999" , 2);
-        physikz.addRandomVelocity(circle, canvas);
-        view.addChild(circle); circles.push(circle);
-    }
-
+        function drawCircle() {
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+          physikz.addRandomVelocity(circle, canvas);
+         view.addChild(circle);
+        circles.push(circle);
+        }
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCirlce();
-        drawCirlce();
+      
+        var loopsCompleted = 0; 
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            drawCircle()
+        }
         
-        
-
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -50,22 +47,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.udatePosition(circles[0])
-            physikz.udatePosition(circles[1])
-            physikz.udatePosition(circles[2])
-            physikz.udatePosition(circles[3])
-            physikz.udatePosition(circles[4])
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           game.checkCirclePosition(circles[0]);
-           game.checkCirclePosition(circles[1]);
-           game.checkCirclePosition(circles[2]);
-           game.checkCirclePosition(circles[3]);
-           game.checkCirclePosition(circles[4]);
-
-            // TODO 9 : Iterate over the array
-           
             
+            // TODO 9 : Iterate over the array
+            for (var i = 0; i < circles.length; i++) {
+                var eachValue = circles[i]
+                var eachCircle = circles[i]
+                physikz.updatePosition(eachCircle)
+                game.checkCirclePosition(eachCircle)
+                
+                // code to repeat using eachValue
+             }
         }
     
         /* 
@@ -78,11 +71,17 @@ var init = function (window) {
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
-            }
-            
+            }    
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            if ( circle.x > canvas.width ) {
+                circle.x = 0;
+            }
+            if ( circle.y < canvas.length ) {
+                circle.y = 0;
+            }
+            if ( circle.y < canvas.length ) {
+                circle.y = 0;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
